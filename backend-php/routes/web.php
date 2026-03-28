3f6d09ce9af99f76f1e4d96a70b07ddd5693b2fd
bayroon10 <ba.raio@duocuc.cl>
2026-03-27 23:37:39 -0300

    👑 Implementando sistema de roles para el panel de Administrador

--- backend-php/routes/web.php
+++ backend-php/routes/web.php
@@ -7,3 +7,6 @@ Route::get('/', function () {
 });
 Route::get('/tienda/productos', [ProductoController::class, 'index']);
 Route::post('/tienda/productos', [App\Http\Controllers\ProductoController::class, 'store']);
+Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return '¡Base de datos formateada y Admin sembrado con éxito! 🚀';
+});
