4596e7a3438672b83a3093b7401a325400e5e1da
bayroon10 <ba.raio@duocuc.cl>
2026-04-25 00:46:02 -0400

    security: apply trustProxies patch and add frontend env example

--- backend-php/bootstrap/app.php
+++ backend-php/bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->validateCsrfTokens(except: [
             'tienda/productos', // <-- Deja pasar a la tienda sin pase
         ]);
