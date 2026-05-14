4f8194c48521336647b31dd2e675f5a3fb80313b
bayroon10 <ba.raio@duocuc.cl>
2026-05-13 21:09:55 -0400

    hotfix: corregir puerto nginx y CORS default en cors.php

--- backend-php/config/cors.php
+++ backend-php/config/cors.php
@@ -5,7 +5,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(explode(',', env('ALLOWED_ORIGINS', 'http://localhost:5173'))),
+    'allowed_origins' => env('ALLOWED_ORIGINS') 
+        ? array_filter(explode(',', env('ALLOWED_ORIGINS'))) 
+        : ['*'],
 
     'allowed_origins_patterns' => [],
 
