482832b320f94c2b9b5605cb4e45d92358b1549c
bayroon10 <ba.raio@duocuc.cl>
2026-08-07 15:55:46 -0400

    chore: add temporary DB::listen diagnostic query logger

--- backend-php/app/Providers/AppServiceProvider.php
+++ backend-php/app/Providers/AppServiceProvider.php
@@ -19,6 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Support\Facades\DB::listen(function ($query) {
+            \Illuminate\Support\Facades\Log::channel('stderr')->info('[SQL-QUERY] ' . $query->sql, [
+                'bindings' => $query->bindings,
+                'time_ms' => $query->time,
+            ]);
+        });
     }
 }
